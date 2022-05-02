b2c5af28a26ceb5f123c4597b37b26c43233fa00
E/S Pronk <eddy@autofilltech.com>
2022-05-02 04:13:59 +0200

    Augment images

--- dataTools/matDataloader.py
+++ dataTools/matDataloader.py
@@ -18,34 +18,38 @@ class matDatasetReader(Dataset):
         self.transformLR = transforms
         self.imageH = height
         self.imageW = width
-        normalize1 = transforms.Normalize(normMean(1), normStd(1))
-        normalize3 = transforms.Normalize(normMean(3), normStd(3))
+        self.normalize1 = transforms.Normalize(normMean(1), normStd(1))
+        self.normalize3 = transforms.Normalize(normMean(3), normStd(3))
 
-        self.transformHRGT = transforms.Compose([transforms.Resize((self.imageH,self.imageW), interpolation=Image.BICUBIC),
-                                                transforms.ToTensor(),
-                                                normalize3,
-                                                ])
+        self.transformHRGT = transforms.Compose([
+                                                transforms.RandomHorizontalFlip(0.5),
+                                                transforms.RandomVerticalFlip(0.5),
+                                                transforms.ColorJitter((1,1.5), 0.3, (1,2), 0.5),
+                                                transforms.RandomRotation(45, Image.BILINEAR),
+						transforms.RandomPerspective()
+						])
 
     
-        self.transformRI = transforms.Compose([transforms.ToTensor(),
-                                                normalize1,
-                                                AddGaussianNoise(0, 0.1, pov=0.6)
+        self.transformRI = transforms.Compose([
+                                                AddGaussianNoise(0, 0.02, pov=1),
                                             ])
 
     def __len__(self):
         return (len(self.image_list))
     
     def __getitem__(self, i):
-
         # Read Images
         #print ("print i",i, i+1)
         raw = loadmat(self.image_list[i])  
-        raw = np.stack((np.asarray(raw["raw"])[:,:,0], np.asarray(raw["raw"])[:,:,1], np.asarray(raw["raw"])[:,:,3]), 0)
-        raw = torch.tensor(raw / 65535.0) ** (1/2.4)
-        
+        raw = np.asarray(raw["raw"]) 
+        raw = np.stack((raw[:,:,0], raw[:,:,1], raw[:,:,3]), 0)
+        raw = torch.tensor(raw / 65535) ** (1/2.4)
+        raw = self.transformHRGT(raw).float()
+        #raw = raw.permute(2, 0, 1)
         # create bayered image straight from the raw original
         # This is for testing a quadbayer format, testing only 1/4 of the channels
         sampled = raw.clone()
+        raw = self.normalize3(raw)
         sampled[0, 0::4, 1::4] = 0
         sampled[0, 0::4, 2::4] = 0
         sampled[0, 0::4, 3::4] = 0
@@ -72,8 +76,8 @@ class matDatasetReader(Dataset):
 	#self.sampledImage = Image.fromarray(np.array((sampled * 255).permute(1,2,0), dtype=np.uint8))
 
         # test storing it in a single channel instead
-        sampled = sampled[0] + sampled[1] + sampled[2] #torch.stack((sampled[0] + sampled[1] + sampled[2],), 0)
-        self.sampledImage = Image.fromarray(np.array((sampled * 255), dtype=np.uint8))
+        sampled = torch.stack((sampled[0] + sampled[1] + sampled[2],), 0)
+	#self.sampledImage = Image.fromarray(np.array((sampled * 255), dtype=np.uint8))
 	
         # This is for creating a regular bayer pattern, instead of the code above
         #sampled[0, 0::2, 1::2] = 0
@@ -84,11 +88,10 @@ class matDatasetReader(Dataset):
         #sampled[2, 1::2, 0::2] = 0
         #self.sampledImage = Image.fromarray(np.array((sampled * 255).permute(1,2,0), dtype=np.uint8))
 
-        self.gtImage = Image.fromarray(np.array((raw * 255).permute(1,2,0), dtype=np.uint8))
-
+	#self.gtImage = Image.fromarray(np.array((raw * 255).permute(1,2,0), dtype=np.uint8))
         # Transforms Images for training 
-        self.inputImage = self.transformRI(self.sampledImage)
-        self.gtImageHR = self.transformHRGT(self.gtImage)
+        self.inputImage = self.normalize1(self.transformRI(sampled))
+        self.gtImageHR = raw #self.gtImage #self.transformHRGT(self.gtImage)
 
         #print (self.gtImageHR.max(), self.gtImageHR.min(), self.inputImage.max(), self.inputImage.min())
 
