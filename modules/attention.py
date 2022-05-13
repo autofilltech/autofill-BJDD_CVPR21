1cd55cbc569bd88a704f9d04984c71c825bd5eca
E/S Pronk <eddy@autofilltech.com>
2022-05-13 03:35:36 +0200

    WIP

--- modules/attention.py
+++ modules/attention.py
@@ -4,9 +4,9 @@ import torch.nn.functional as F
 import numpy as np
 
 # Squeeze and Excitation Layer
-class SELayer(nn.Module):
+class SqueezeExcitationLayer(nn.Module):
 	def __init__(self, channels, reduction = 16, bias = False):
-		super(SELayer, self).__init__()
+		super(SqueezeExcitationLayer, self).__init__()
 		self.pool = nn.AdaptiveAvgPool2d(1)
 		self.body = nn.Sequential(
 				nn.Linear(channels, channels // reduction, bias = bias),
@@ -20,38 +20,73 @@ class SELayer(nn.Module):
 		return x + y.expand_as(x)
 
 # Spatial Attention Layer
-class SALayer(nn.Module):
+class SpatialAttentionBlock(nn.Module):
 	def __init__(self, kernel_size = 3, bias = False):
-		super(SALayer, self).__init__()
+		super(SpatialAttentionBlock, self).__init__()
 		self.body = nn.Sequential(
-				FeaturePool2d(),
+				MuxCat(
+					ChannelMaxPool2d(),
+					ChannelAvgPool2d(),
+					dim = -3),
 				nn.Conv2d(2, 1, kernel_size, padding = kernel_size // 2, bias = bias)
 				nn.Sigmoid())
 
 # Attention Guided Residual Block
-class AttResBlock(nn.Module):
-	def __init__(self, channels = 32, expand = None, dilation = 1, bias = True):
-		super(AttResBlock, self).__init__()
-		if expand is None: expand = channels * 2
-		self.datt = SELayer(channels)
-		self.body = nn.Sequential(
-			nn.Conv2d(channels, expand, 1),
-			nn.BatchNorm2d(expand),
-			nn.ReLU(inplace = True),
-			nn.Conv2d(expand, expand, kernel_size = 3, dilation = dilation, padding = dilation),
-			nn.BatchNorm2d(expand),
-			nn.ReLU(inplace = True),
-			nn.Conv2d(expand, channels, 1))
+class AttentionResBlock(nn.Sequential):
+	def __init__(self, channels = 32, expand = 64):
+		super(AttentionResBlock, self).__init__(
+			MuxAdd(
+				Identity(),
+				SqueezeExcitationLayer(channels),
+				nn.Sequential(
+					nn.Conv2d(channels, expand, 1),
+					nn.BatchNorm2d(expand),
+					nn.ReLU(inplace = True),
+					nn.Conv2d(expand, expand, kernel_size = 3),
+					nn.BatchNorm2d(expand),
+					nn.ReLU(inplace = True),
+					nn.Conv2d(expand, channels, 1))))
 		
-	def forward(self, x):
-		a = self.datt(x)
-		y = self.body(x)
-		return x + y + a
 
-class FeaturePool2d(nn.Module):
+class ChannelMaxPool2d(nn.Module):
 	def forward(self, x):
 		xmax,_ = torch.max(x, dim=-3, keep_dim=True)
-		xmean = torch.mean(x, dim=-3, keep_dim=True)
-		return torch.cat((xmax, xmean), dim=1)
+		return xmax
 
+class ChannelAvgPool2d(nn.Module):
+	def forward(self, x):
+		xmean = torch.mean(x, dim=-3, keep_dim=True)
+		return xmean
+
+class ChannelAttentionBlock(nn.Sequential):
+	def __init__(self, channels, reduction=8, bias=False):
+		super(ChannelAttentionBlock, self).__init__(
+			MuxAdd(
+				nn.Sequential(
+					nn.MaxPool2d(2),
+					nn.Conv2d(channels, channels//reduction, 1, bias=bias),
+					nn.ReLU(inplace=True),
+					nn.Conv2d(channels//reduction, channels, 1, bias=bias)),
+				nn.Sequential(
+					nn.AvgPool2d(2),
+					nn.Conv2d(channels, channels//reduction, 1, bias=bias),
+					nn.Relu(inplace=True),
+					nn.Conv2d(channels//reduction, channels, 1, bias=bias))),
+			nn.Sigmoid())
+
+
+# Oc = C(x) * x
+# y = S(Oc) * Oc + x
+class RCSABlock(nn.Sequential):
+	def __init__(self, channels):
+		super(RCSABlock, self),__init__(
+			MuxAdd(
+				Identity(),
+				nn.Sequential(
+					MuxMul(
+						ChannelAttentionBlock(channels),
+						Identity),
+					MuxMul(
+						SpatialAttentionBlock(),
+						Identity)))
 
--- modules/common.py
+++ modules/common.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class SeparableConv2d(nn.Sequential):
 	def __int__(self, features_in, features_out, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
@@ -18,5 +19,61 @@ class SeparableConv2d(nn.Sequential):
 		
 		super(SeparableConv2D, self).__init__(
 
+class View(nn.Module):
+	def __init__(self, *args):
+		super(View, self).__init__()
+		self.shape = args
+	
+	def forward(self, x):
+		return x.view(*self.shape)
+
+class Identity(nn.Module):
+	def forward(self, x):
+		return x
+
+class MuxAdd(nn.Module):
+	def __init__(self, *args, **kwargs):
+		super(MuxSum, self).__init__()
+		self.mux = args
 		
+	def forward(self, x):
+		outputs = []
+		for module in self.mux:
+			outputs.append(module(x))
+		return outputs.sum()
+
+class MuxMul(nn.Module):
+	def __init__(self, *args, **kwargs):
+		super(MuxSum, self).__init__()
+		self.mux = args
+		
+	def forward(self, x):
+		outputs = []
+		for module in self.mux:
+			outputs.append(module(x))
+		return np.prod(outputs)
+
+class MuxSum(nn.Module):
+	def __init__(self, *args, **kwargs):
+		super(MuxSum, self).__init__()
+		self.mux = args
+		self.dim = kwargs["dim"] if "dim" in kwargs else 0
+		
+	def forward(self, x):
+		outputs = []
+		for module in self.mux:
+			outputs.append(module(x))
+		return torch.sum(outputs, dim=self.dim)
+
+class MuxCat(nn.Module):
+	def __init__(self, *args, **kwargs):
+		super(MuxCat, self).__init__()
+		self.mux = args
+		self.dim = kwargs["dim"] if "dim" in kwargs else 0
+		
+	def forward(self, x):
+		outputs = []
+		for module in self.mux:
+			outputs.append(module(x))
+		return torch.cat(outputs, dim=self.dim)
 
